77aa0da0609ff5227b62027022f1a4a4b869033b
Otabek <abdullayevriza80@gmail.com>
2026-02-19 18:51:24 +0500

    wert

--- js/index.js
+++ js/index.js
@@ -1,11 +1,20 @@
 const card=document.getElementById("card")
-
+console.log(card)
 fetch("https://dummyjson.com/products")
 .then(response => response.json())
 .then(date => {
-    const div=document.createElement("div")
-
+    
+    
     date.products.forEach(item =>{
-        
+        const div=document.createElement("div")
+        div.className="wewe"
+
+        div.innerHTML=`
+            <img class="imgs" src="${item.thumbnail}" alt="">
+            <h3 class="textr" >${item.title}</h3>
+            <p class="textre" >${item.price}</p>
+        `
+        card.appendChild(div)
     })
-})
+    
+}).catch(error => {console.error("xato".error)})
